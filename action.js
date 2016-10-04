328ab40ef1e6c0f7ff7da548315a725e6f25031f
uracilo <benjamin.casazza@gmail.com>
2016-10-03 20:56:02 -0500

    primer commit

---
+++ action.js
@@ -0,0 +1,20 @@
+console.log('test');
+
+
+var respuesta = prompt ( "Juega piedra papel o tijera" );
+
+var piedra;
+
+
+
+if (piedra === respuesta) {
+	respuesta == "Piedra" && "piedra";
+	document.write("Pusiste "+ respuesta +" yo digo " + "piedra");
+
+} else  {
+	console.log("else");
+	respuesta == piedra
+		document.write(" Yo tambien tire piedra volvamos a jugar");
+
+}
+
