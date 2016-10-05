335dea160cb5b9bcaa1296361b546d38651e09ab
uracilo <benjamin.casazza@gmail.com>
2016-10-04 19:42:55 -0500

    ahora a comprobar una contraseña

--- action.js
+++ action.js
@@ -1,18 +1,26 @@
+/*
 console.log('test');
 
 
 var respuesta = prompt ( "Juega piedra papel o tijera" );
 
-var piedra;
+var piedra = "piedra";
+var tijera = "tijera";
+var papel = "papel";
 
-if (piedra === respuesta) {
-	respuesta == "Piedra" && "piedra";
-	document.write("Pusiste "+ respuesta +" yo digo " + "piedra");
+console.log(piedra);
 
-} else  {
-	console.log("else");
-	respuesta == piedra
-		document.write(" Yo tambien tire piedra volvamos a jugar");
+if (piedra != respuesta) {
+document.write("Pusiste "+ respuesta +" yo digo " + "piedra y perdí");
+
+}else{
+document.write(" Yo tambien tire piedra volvamos a jugar empatamos");
 
 }
+if (piedra = tijera && piedra == respuesta) {
+	document.write(" Pusiste "+ respuesta +" yo digo piedra gané");
+
+}
+
+*/
 
