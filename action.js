f9d9141b43dc8dc5279cab6eaee17ab6addbefac
uracilo <benjamin.casazza@gmail.com>
2016-10-04 21:29:35 -0500

    ahora es el gato

--- action.js
+++ action.js
@@ -24,13 +24,39 @@ if (piedra = tijera && piedra == respuesta) {
 
 */
 
-
+/*
 do {
 	var contrasena = prompt ("ingresa tu contraseña");
 	//document.write("esta no es tu contraseña nooooo!!!!");
 
 }while (contrasena !== "contraseña" );
 document.write(contrasena);
+*/
+var juego_compu=["papel","piedra","tijeras"];
+var tomas_elseñor= prompt("¿PIEDRA,PAPEL O TIJERA?");
+console.log("tomas:"+tomas_elseñor);
+
+numero_random=parseInt(Math.random()*3);
+console.log(numero_random);
+
+pepe=juego_compu[numero_random];
+ console.log("pepe:"+pepe)
+
+// situacion numero 1
+if (tomas_elseñor == pepe) {
+
+ console.log("empate")
+}
+
+// situacion numero 2 perdemos
+
+if ((tomas_elseñor == "piedra" && pepe == "papel") || (tomas_elseñor == "tijeras" && pepe == "piedra") || (tomas_elseñor == "papel" && pepe== "tijeras") ) {
+ console.log("pepe te gano")
+}
+
+// situacion numero 3 ganamos
 
 
-
+if ((tomas_elseñor == "papel" && pepe == "piedra") || (tomas_elseñor == "piedra" && pepe == "tijeras") || (tomas_elseñor == "tijeras" && pepe== "papel") ) {
+ console.log("pepe perdio")
+}
